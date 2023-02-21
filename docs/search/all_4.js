07395c64e092f57801a59de14c762b22c72e255b
Carl Timmer <timmer@jlab.org>
2023-02-21 14:00:54 +0000

    Update docs

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['focus_24',['focus',['../classsnake_1_1widget_1_1Button.html#a93442843bf42cccab823126f5d40dbf3',1,'snake::widget::Button']]],
   ['font_25',['font',['../classsnake_1_1state_1_1State.html#a34f9881f86ba3c48cca5c1bdcf633207',1,'snake::state::State']]],
   ['food_26',['Food',['../classsnake_1_1entity_1_1Food.html#a4ec897d8df22349be8ff080d9278d205',1,'snake::entity::Food']]],
-  ['food_27',['food',['../classsnake_1_1entity_1_1Food.html#a0ed77f260b4def38545785ee1250da2e',1,'snake::entity::Food']]],
+  ['food_27',['food',['../classsnake_1_1entity_1_1Food.html#a81dca28157972bf258d5623a38c313d3',1,'snake::entity::Food']]],
   ['food_28',['Food',['../classsnake_1_1entity_1_1Food.html',1,'snake::entity']]],
   ['food_2ecpp_29',['food.cpp',['../food_8cpp.html',1,'']]],
   ['food_2ehpp_30',['food.hpp',['../food_8hpp.html',1,'']]]
--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['focus_193',['focus',['../classsnake_1_1widget_1_1Button.html#a93442843bf42cccab823126f5d40dbf3',1,'snake::widget::Button']]],
   ['font_194',['font',['../classsnake_1_1state_1_1State.html#a34f9881f86ba3c48cca5c1bdcf633207',1,'snake::state::State']]],
-  ['food_195',['food',['../classsnake_1_1entity_1_1Food.html#a0ed77f260b4def38545785ee1250da2e',1,'snake::entity::Food']]]
+  ['food_195',['food',['../classsnake_1_1entity_1_1Food.html#a81dca28157972bf258d5623a38c313d3',1,'snake::entity::Food']]]
 ];
