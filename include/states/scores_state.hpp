5371e78d79135117bfc1920221d23c8f54aeff95
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2023-03-03 10:35:42 +0100

    Finalized the scores state.

--- include/states/scores_state.hpp
+++ include/states/scores_state.hpp
@@ -28,6 +28,7 @@
 
 // STD
 #include <vector>
+#include <map>
 
 namespace snake::state{
 
@@ -60,14 +61,24 @@ namespace snake::state{
             void drawImg();
             void drawWidgets() override;
             void packWidgets() override;
+            std::multimap<uint64_t, std::string, std::greater<int64_t>> getScoresMap();
+            void initStrings();
 
             // Variables
             window::GameWindow* game_window;
             sf::Text back_to_menu;
-            std::vector<uint64_t> scores;
+            sf::Text first_player;
+            sf::Text second_player;
+            sf::Text third_player;
+            std::multimap<uint64_t, std::string, std::greater<int64_t>> scores_map;
             sf::Sprite gold_trophy;
             sf::Sprite silver_trophy;
             sf::Sprite bronze_trophy;
+            sf::Sprite title;
+            std::string string_first;
+            std::string string_second;
+            std::string string_third;
+            std::multimap<uint64_t, std::string, std::greater<int64_t>>::iterator iterator;
 
             // Constants
             const uint64_t game_window_size_x = this -> game_window -> getSize().x;
--- src/states/scores_state.cpp
+++ src/states/scores_state.cpp
@@ -54,14 +54,53 @@ namespace snake::state{
      * @param game_window The window to which the state stuff is constructed.
      */
     ScoresState::ScoresState( window::GameWindow* game_window ): 
-        game_window( game_window ){
+        game_window( game_window ),
+        string_first( "" ),
+        string_second( "" ),
+        string_third( "" ){
 
         // Compute best scores
-        this -> scores = this -> game_window -> scores_container;
-        std::sort( scores.begin(), scores.end(), std::greater<uint64_t>() );
+        this -> scores_map = this -> getScoresMap();
 
         // Draw widgets
         this -> drawWidgets();
+    }
+
+    //====================================================
+    //     getScoresContainer
+    //====================================================
+    /**
+     * @brief Method used to get the scores container with also player names.
+     * 
+     */
+    std::multimap<uint64_t, std::string, std::greater<int64_t>> ScoresState::getScoresMap(){
+
+        // Read scores from file
+        std::ifstream score_file( this -> game_window -> score_file_path );
+        if( ! score_file ){
+            std::ofstream default_settings( this -> game_window -> score_file_path );
+            default_settings.close();
+        }
+        std::string score_line;
+        std::vector<std::string> lines;
+        while( std::getline( score_file, score_line ) ){
+            lines.push_back( score_line );
+        }
+
+        // Compute best score
+        std::multimap <uint64_t, std::string, std::greater<int64_t>> scores_map;
+        std::string player_name;
+        uint64_t player_score;
+        for( const auto& line: lines ){
+            std::stringstream words( line );
+            words >> player_score >> player_name;
+            scores_map.insert( { player_score, player_name } );
+        }
+
+        // Close stream
+        score_file.close();
+
+        return scores_map;
     }
 
     //====================================================
@@ -103,13 +142,16 @@ namespace snake::state{
         if( ! state_texture_2.loadFromFile( "img/images/menu_background.jpg" ) ){
             this -> game_window -> close();
         }
-        if( ! state_texture_3.loadFromFile( "img/textures/gold_trophy.png" ) ){
+        if( ! state_texture_3.loadFromFile( "img/textures/gold_icon.png" ) ){
             this -> game_window -> close();
         }
-        if( ! state_texture_4.loadFromFile( "img/textures/silver_trophy.jpeg" ) ){
+        if( ! state_texture_4.loadFromFile( "img/textures/silver_icon.png" ) ){
             this -> game_window -> close();
         }
-        if( ! state_texture_5.loadFromFile( "img/textures/bronze_trophy.jpeg" ) ){
+        if( ! state_texture_5.loadFromFile( "img/textures/bronze_icon.png" ) ){
+            this -> game_window -> close();
+        }
+        if( ! state_texture_6.loadFromFile( "img/images/scores_title.png" ) ){
             this -> game_window -> close();
         }
 
@@ -124,25 +166,32 @@ namespace snake::state{
         this -> background.setSize( sf::Vector2f( this -> game_window_size_x, this -> game_window_size_y ) );
         this -> background.setTexture( &this -> state_texture_2, true );
 
+        // Title image
+        this -> title.setTexture( this -> state_texture_6 );
+        this -> title.setPosition( 
+           ( this -> game_window_size_x * 0.5f - this -> title.getGlobalBounds().width * 0.5f ), 
+           ( this -> game_window_size_y * 0.5f - this -> title.getGlobalBounds().height * 0.5f ) * 0.2f
+        );
+
         // First award icon
         this -> gold_trophy.setTexture( this -> state_texture_3 );
         this -> gold_trophy.setPosition( 
-           ( this -> game_window_size_x * 0.5f - this -> width * 0.5f ) * 0.75, 
-           ( this -> game_window_size_y * 0.5f - this -> height * 0.5f ) * 0.9f
+           ( this -> game_window_size_x * 0.5f - this -> title.getGlobalBounds().width * 0.5f ), 
+           ( this -> game_window_size_y * 0.5f - this -> gold_trophy.getGlobalBounds().height * 0.5f )
         );
 
         // Second award icon
         this -> silver_trophy.setTexture( this -> state_texture_4 );
         this -> silver_trophy.setPosition( 
-           this -> gold_trophy.getPosition().x, 
-           this -> gold_trophy.getPosition().y * 1.3f
+           this -> gold_trophy.getPosition().x * 1.023f, 
+           this -> gold_trophy.getPosition().y * 1.35f
         );
 
         // Third award icon
         this -> bronze_trophy.setTexture( this -> state_texture_5 );
         this -> bronze_trophy.setPosition( 
-           this -> gold_trophy.getPosition().x, 
-           this -> silver_trophy.getPosition().y * 1.22f
+           this -> silver_trophy.getPosition().x, 
+           this -> silver_trophy.getPosition().y * 1.225f
         );
 
         // Drawing the images
@@ -151,6 +200,7 @@ namespace snake::state{
         this -> game_window -> draw( gold_trophy );
         this -> game_window -> draw( silver_trophy );
         this -> game_window -> draw( bronze_trophy );
+        this -> game_window -> draw( title );
     }
 
     //====================================================
@@ -171,10 +221,62 @@ namespace snake::state{
         this -> back_to_menu.setCharacterSize( this -> text_size + 6 );
 
         // First award player name
+        this -> first_player.setFillColor( this -> textColor );
+        this -> first_player.setPosition( 
+            ( this -> game_window_size_x * 0.5f - this -> title.getGlobalBounds().width * 0.5f ) * 0.95f,
+            ( this -> game_window_size_y * 0.5f - this -> title.getGlobalBounds().height * 0.5f ) * 0.94f
+        );
+        this -> first_player.setCharacterSize( this -> text_size + 14 );
 
         // Second award player name
+        this -> second_player.setFillColor( this -> textColor );
+        this -> second_player.setPosition( 
+            ( this -> game_window_size_x * 0.5f - this -> title.getGlobalBounds().width * 0.5f ) * 0.95f,
+            ( this -> game_window_size_y * 0.5f - this -> title.getGlobalBounds().height * 0.5f ) * 1.22f
+        );
+        this -> second_player.setCharacterSize( this -> text_size + 14 );
 
         // Third award player name
+        this -> third_player.setFillColor( this -> textColor );
+        this -> third_player.setPosition( 
+            ( this -> game_window_size_x * 0.5f - this -> title.getGlobalBounds().width * 0.5f ) * 0.95f,
+            ( this -> game_window_size_y * 0.5f - this -> title.getGlobalBounds().height * 0.5f ) * 1.49f
+        );
+        this -> third_player.setCharacterSize( this -> text_size + 14 );
+    }
+
+    //====================================================
+    //     initStrings
+    //====================================================
+    /**
+     * @brief Method used to initialize scores strings.
+     * 
+     */
+    void ScoresState::initStrings(){
+        try{
+            this -> iterator = this -> scores_map.begin();
+            this -> string_first = std::to_string( iterator -> first ) + "   " + iterator -> second;
+            try{
+                this -> iterator++;
+                this -> string_second = std::to_string( iterator -> first ) + "   " + iterator -> second;
+                try{
+                    this -> iterator++;
+                    this -> string_third = std::to_string( iterator -> first ) + "   " + iterator -> second;
+                }
+                catch( const std::bad_alloc& ){
+                    this -> string_third = "Empty";
+                }
+            }
+            catch( const std::bad_alloc& ){
+                this -> string_second = "Empty";
+                this -> string_third = "Empty";
+            }
+        }
+        catch( const std::bad_alloc& ){
+            this -> string_first = "Empty";
+            this -> string_second = "Empty";
+            this -> string_third = "Empty";
+        }
     }
 
     //====================================================
@@ -190,13 +292,25 @@ namespace snake::state{
         this -> back_to_menu.setFont( this -> font );
         this -> back_to_menu.setString( "Press <Tab> to back to menu" );
 
+        // Modify strings
+        this -> initStrings();
+
         // First award player name settings
+        this -> first_player.setFont( this -> font );
+        this -> first_player.setString( this -> string_first );
 
         // Second award player name settings
+        this -> second_player.setFont( this -> font );
+        this -> second_player.setString( this -> string_second );
 
         // Third award player name settings
+        this -> third_player.setFont( this -> font );
+        this -> third_player.setString( this -> string_third );
 
         // Draw stuff
         this -> game_window -> draw( this -> back_to_menu );
+        this -> game_window -> draw( this -> first_player );
+        this -> game_window -> draw( this -> second_player );
+        this -> game_window -> draw( this -> third_player );
     }
 }
